825a07ed246572dfca9d627a946f11e1b0e5760f
Velan1508 <uktvelan@gmail.com>
2024-11-12 13:23:51 +0530

    Initial Commit

---
+++ GoodsDescription.py
@@ -0,0 +1,20 @@
+from client import RiskAzureChatOpenAI 
+from client import RiskAzureOpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_community.document_loaders import PyPDFLoader
+import json
+from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+load_dotenv()
+
+api_key = "1d6bf084823a4a93b30a103a7f035046"
+DOC_PATH = "document.json"
+
+embeddings = RiskAzureOpenAIEmbeddings(openai_api_key=api_key)
+llm = RiskAzureChatOpenAI(openai_api_key=api_key, temperature=0.)
+description=""
+test_llm_result = llm.invoke("Generate a keyword most suitable for this description - Protective and decontamination goods, specially designed or modified for military use, components and chemical mixtures as follows")
+print(test_llm_result)
